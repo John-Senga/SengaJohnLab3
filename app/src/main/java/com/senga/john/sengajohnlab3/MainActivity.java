dd077786ba427353431f3389e728ee54cebddb1a
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-09-04 21:14:44 +0800

    Changed activity title

--- app/src/main/java/com/senga/john/sengajohnlab3/MainActivity.java
+++ app/src/main/java/com/senga/john/sengajohnlab3/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		setTitle("Styles & Material Design");
         setContentView(R.layout.constraint_layout);
     }
 }
